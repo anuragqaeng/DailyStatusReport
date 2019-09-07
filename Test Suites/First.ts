10682ad13b0dda71b6276cff709f8403e6532c25
Anurag Singh <anuragsingh.microsoft@gmail.com>
2019-09-08 03:37:54 +0530

    First Commit

---
+++ Test Suites/First.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>First</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5940445b-70e4-43c4-a12c-9a9400eda464</testSuiteGuid>
+</TestSuiteEntity>
